c8356480d96283a1c88d87f59782e798c5f4ac52
Matt Hefner <matt.r.hefner@gmail.com>
2019-09-10 15:41:40 -0700

    customer and manager ports edited

--- bamazonManager.js
+++ bamazonManager.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 // Define the MySQL connection parameters
 var connection = mysql.createConnection({
 	host: 'localhost',
-	port: 8080,
+	port: 3306,
 
 	// Your username
 	user: 'root',
